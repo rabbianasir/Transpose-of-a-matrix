2942e8374643008180b56ead932d7840741cd8b7
Abhijit-codder <111674298+Abhijit-codder@users.noreply.github.com>
2023-03-05 23:46:48 +0530

    Update tranpose.c
    Corrected the program transpose of matrix and added meaningful variables

--- tranpose.c
+++ tranpose.c
@@ -1,20 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int matrix1[2][2],i,j;
+    int matrix1[2][2],i,j,transpose[2][2];
+     printf("enter the elements in a matrix");
+    //input from user
     for(i=0;i<2;i++)
     {
-        printf("enter the elements of a matrix in a row:");
         for(j=0;j<2;j++)
+        {
+            
             scanf("%d",&matrix1[i][j]);
+        }
     }
 
-    puts("TRANSPOSE OF MATRIX:");
-
-    for(i=0;i<2;i++)
+    for(i=0;i<2;i++) //computing the transpose
     {
         for(j=0;j<2;j++)
-            printf("\t%d",matrix1[j][i]);
+        {
+           transpose[j][i]= matrix1[i][j]; 
+        }
+    }
+    //printing the transpose of matrix
+    for(i=0;i<2;i++) 
+    {
+        for(j=0;j<2;j++)
+        {
+           printf("%d",transpose[i][j]);
+                  
+        }
         printf("\n");
     }
+    return 0;
 }
